6f7a62269b049412946c59955d796c18593c949c
Mujahid <mujahid.ali@innobles.com>
2020-09-01 10:50:06 +0530

    IBDesignable and IBInspactable

---
+++ IBDesignableDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IBDesignableDemo
+//
+//  Created by Mujahid on 01/09/20.
+//  Copyright © 2020 Mujahid. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
